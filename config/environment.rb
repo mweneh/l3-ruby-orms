c2263cd8c3fe3d5e8eaca93a8c2f6548df00e63d
David <davidongaroh@gmail.com>
2022-08-24 14:10:24 +0300

    done

--- config/environment.rb
+++ config/environment.rb
@@ -6,3 +6,23 @@ require_relative '../lib/student'
 DB = { conn: SQLite3::Database.new("db/school.db") }
 
 # RUN CODE FROM HERE
+
+Student.create_table
+
+kelvin = Student.new(name:"Kelvin",age:22)
+# add student
+kelvin.add_to_db
+pp Student.all
+kelvin.name = "Kevoh"
+kelvin.age = 24
+kelvin.update
+
+pp Student.all
+
+kelvin.name = "water loo"
+kelvin.age= 19
+kelvin.add_to_db
+
+pp Student.all
+
+kelvin.delete
--- lib/student.rb
+++ lib/student.rb
@@ -4,21 +4,90 @@ class Student
     def initialize(name:, age:, id: nil)
         @id=id
         @name=name
-        @id=id
+        @age=age
     end
 
-# TODO: CREATE TABLE
+# TODO: CREATE TABLE-class method
+def self.create_table
+    sql = <<-SQL
+    CREATE TABLE if NOT EXISTS students(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        age INTEGER
+    )
+    SQL
+    DB[:conn].execute(sql)
+end
 
-# TODO: INSERT RECORD
+# TODO: INSERT RECORD - instance method
+def add_to_db
+    sql = <<-SQL
+    INSERT INTO students (name,age)
+    VALUES(?,?)
+    SQL
+    DB[:conn].execute(sql,self.name,self.age)
+    set_id
+end
 
-# TODO: SHOW ALL RECORDS
+# TODO: SHOW ALL RECORDS-class method
+def self.all
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    DB[:conn].execute(sql).map do |row|
+     object_from_db(row)
+    end
+end
+# SHOW PREVIOUSLY ADDED
 
+def show_most_recent
+    sql = <<-SQL
+    SELECT * FROM students ORDER BY id DESC LIMIT 1 
+    SQL
+    DB[:conn].execute(query)map do |row|
+        Student.object_from_db(row)
+    end.first
+end
 # TODO: UPDATE RECORD
-
+def update
+    sql = <<-SQL
+    UPDATE students
+    SET name = ?,age = ?
+    WHERE id = ?
+    SQL
+    DB[:conn].execute(sql,self.name,self.age,self.last_insert_rowid)
+    
+end
 # TODO: DELETE RECORD
 
-# TODO: CONVERT TABLE RECORD TO RUBY OBJECT
+def delete
+    sql = <<-SQL
+    DELETE FROM students WHERE id = ?
+    SQL
+    DB[:conn].execute(sql,self.id)
+end
+# TODO: CONVERT TABLE RECORD TO RUBY OBJECT-class
+def self.object_from_db(row)
+    self.new(id:[0],name:row[1],age:row[2])
+end
 
-# TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS
+# TODO: SEARCH FOR RECORD THAT MEETS CERTAIN CONDITIONS -OLDEST
+def self.find_oldest
+    sql = <<-SQL
+    SELECT * FROM students ORDER BY age DESC LIMIT 1
+    SQL
+    DB[:conn].execute(sql).map do |row|
+        self.object_from_db(row)
+    end.first
+
+    
+end
+
+private
+
+def set_id
+    sql = "SELECT last_insert_rowid() FROM students"
+    self.id = DB[:conn].execute(sql)[0][0]
+end
 
 end
